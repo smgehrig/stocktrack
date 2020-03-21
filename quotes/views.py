0cafc0adf7588cc08959a51dff723536ed93d269
smgehrig <smgehrig@comcast.net>
2020-03-21 11:51:36 -0400

    Adding stock api

--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+import requests
+import json
 
 def home(request):
-	return render(request, 'home.html', {})
+
+	if request.method == 'POST':
+		ticker = request.POST['tickerSearch']
+		api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_37cb8dfc7f334848be40742284eef319")
+		try:
+			api = json.loads(api_request.content)
+		except Exception as e:
+			api = "Error..."
+		return render(request, 'home.html', {'api': api})
+	else:
+		return render(request, 'home.html', {'ticker': "Enter a ticker Symbol"})
+
+
+def settings(request):
+	return render(request, 'settings.html', {})
